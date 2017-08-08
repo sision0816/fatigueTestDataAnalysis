44554a0c0d8e5e7c9f3d64388af00738ea8264f4
sision0816 <chenwen816@gmail.com>
2017-08-08 12:45:44 +0200

    Update 20170725hysteresisMechanicalanalysis.py

--- 20170725hysteresisMechanicalanalysis.py
+++ 20170725hysteresisMechanicalanalysis.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep 17 18:48:41 2016
+Created on Mon Aug 07 11:09:03 2017
 
 @author: chen_w1
 """
+
 
 import pandas as pd
 import numpy as np
@@ -16,9 +17,9 @@ import scipy.stats
 #print 'Input the file name you want to analysis:' 
 readFile_timeSequence = 'experimentTimeData.csv' # in with csv format and in list
 #print 'Input the file name of first cycle:' 
-readFile_firstCycle =  'Nr19_cycle1_1kN.csv'
+readFile_firstCycle =  'Nr17_cycle1_1kN.csv'
 #print 'Input the file name you want to write the after mechanical analysis data:'
-writeFile_mechanicalAnalysisData = 'experimentHysteresisMechanicalAanalysis.csv'
+writeFile_mechanicalAnalysisData = 'experimentHysteresisMechanicalAanalysisTest.csv'
 
 #==============================================================================
 # read the csv data file into dataframe
@@ -48,11 +49,11 @@ df['Strain']=(df['Dehnung_fullrange_SH46 mm']-extensormeterInitialValue)/(gageLe
 #==============================================================================
 
 # determine the largest cycle number
-maxCycle=int(df['Zyklus'].max())
+#maxCycle=int(df['Zyklus'].max())
 # define the output dataframe of max min data
 dfOutput = pd.DataFrame(columns = ['Cycle','Stress Max MPa','Stress Min MPa','Stress Amplitude MPa','Stress Mean MPa','Strain Max','Strain Min','Strain Amplitude','Strain Mean', 'Elastic Modulus GPa', 'Yield Stress MPa','Elastic Strain','Plastic Strain', 'Effective Stress MPa','Back Stress MPa'])
 
-for cycle in range (1, maxCycle+1):
+for cycle in range (1, 2):
    loop=df[df.Zyklus==cycle]
    strainMax = loop['Strain'].max()
    strainMin = loop['Strain'].min()
@@ -67,20 +68,26 @@ for cycle in range (1, maxCycle+1):
 # regression the linear part, calculate elastic modulus
    eModulusSum = 0
    i = 0
-   for eModulusRegressionCount in range (10,len(loop.index)/4):
+   fittingLength = 10 #set the minimum fitting length
+   fittingStartPoint = maxStrainCount + 10 #start fitting from the  maxStrainCount + 10
+   for eModulusRegressionCount in range (10,len(loop.index)/4): 
        if loop.index.max()< maxStrainCount+eModulusRegressionCount: #if the last cycle is not long enough will cause error report in the regression, if the loop length not enough, break
            break
-       xx = loop.loc[maxStrainCount+5:maxStrainCount+eModulusRegressionCount, :]['Strain'] #try to get a series not df or will get error report 'NotImplementedError: Only 2-level MultiIndex are supported.'
-       yy = loop.loc[maxStrainCount+5:maxStrainCount+eModulusRegressionCount, :]['Stress MPa'] #set tge first fitting starting point is maxStrainCount+10 and end point is maxStrainCount+10
+       xx = loop.loc[fittingStartPoint:fittingStartPoint+eModulusRegressionCount, :]['Strain'] #try to get a series not df or will get error report 'NotImplementedError: Only 2-level MultiIndex are supported.'
+       yy = loop.loc[fittingStartPoint:fittingStartPoint+eModulusRegressionCount, :]['Stress MPa'] #set tge first fitting starting point is maxStrainCount+10 and end point is maxStrainCount+10
        slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(xx,yy)
        eModulus = 0.001*slope 
        intercept = intercept
-       if r_value**2<0.96:
-            break
-       eModulusRegressionCount=+1
-       eModulusSum=+eModulus
-       i=+1
-       eModulusAve = eModulusSum/i
+       print r_value**2
+       eModulusRegressionCount+=1
+       if r_value**2>=0.999:
+           eModulusSum+=eModulus
+           i+=1
+           eModulusAve = eModulusSum/i
+       print eModulus
+       if i>1 and r_value**2<0.999:
+           break
+       print eModulusAve
 # count the acquisition point per cycle
    counts=len(loop.index)
 # itterate each data point, yield stress, effective stress and back stress 
@@ -105,6 +112,3 @@ for cycle in range (1, maxCycle+1):
 dfOutput.to_csv(writeFile_mechanicalAnalysisData, sep=';', index = False)
 
 print 'finish'   
-
-
-
