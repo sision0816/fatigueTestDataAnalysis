ad0bc6d06e8911c5fd0bf9807d741e611173515b
sision0816 <chenwen816@gmail.com>
2017-09-19 17:49:39 +0200

    update with loop shape parameter plotting

--- 20170731hysteresisMechanicalAnalysisPlotting.py
+++ 20170731hysteresisMechanicalAnalysisPlotting.py
@@ -1,45 +1,48 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 15 11:32:37 2017
+Created on Wed Aug 02 15:45:24 2017
 
 @author: chen_w1
 """
+
+#==============================================================================
+# import the modules
+#==============================================================================
 import pandas as pd
 import numpy as np
 from pandas import DataFrame, read_csv,read_excel
 import scipy
-import scipy.stats
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
 
-def partsBinding (n):# n is parts number
 #==============================================================================
-# input the read file name and write file name
+# read file
 #==============================================================================
-    for i in range(1,n+1):
-        print 'Type in the {}th part file name, including .csv'.format(i)
-        globals()['readFile_timeSequence_part{}'.format(i)]  = input() #type in the parts file name with '' and .csv, it is very important
-    print 'Type in the output file name, including .csv'
-    writeFile_timeSequence_binded = input()
-#==============================================================================
-# read the csv data file into dataframe
-#==============================================================================
-    for j in range(1,n+1):
-        globals()['df_part{}'.format(j)] = pd.read_csv(globals()['readFile_timeSequence_part{}'.format(j)],sep=';')
-    df_binded = df_part1
-#==============================================================================
-# binding the parts dataframe together
-#==============================================================================
-    for k in range(2,n+1):
-        globals()['df_part{}'.format(k)]['Zeit s'] +=globals()['df_part{}'.format(k-1)]['Zeit s'].max()
-        globals()['df_part{}'.format(k)]['Zyklus'] +=globals()['df_part{}'.format(k-1)]['Zyklus'].max()
-        df_binded = pd.concat([df_binded,globals()['df_part{}'.format(k)]],ignore_index=True)
-#==============================================================================
-# write the binded .csv file
-#==============================================================================
-    df_binded.to_csv(writeFile_timeSequence_binded, sep=';', index = False)
+readFile_mechanicalAnalysisData = 'experimentStrainEnergyCalculationResult.csv'
+df=pd.read_csv(readFile_mechanicalAnalysisData,sep=';')
+#maximum stress
+plt.figure(1)
+#plt.subplot(321)
+plt.title('Strain Energy vs. Cycle')
+plt.plot(df['Cycle'],df['Total Cyclic Strain Energy_trapz MJ/m3'],'k-',label='Total strain energy')
+plt.plot(df['Cycle'],df['Plastic Strain Energy_trapz MJ/m3'],'r-',label='Plastic strain energy')
+plt.plot(df['Cycle'],df['Elastic Strain Energy MJ/m3'],'g-',label='Elastic strain energy')
+plt.ylabel('Strain energy MJ/m^3')
+plt.xlabel('Cycle [-]')
+plt.ylim(0,4)
+plt.xscale('log')
+plt.legend(loc='best')
 
-    print 'finish'   
-    return;
+plt.show()
 
-        
-    
-        
+plt.figure(1)
+#plt.subplot(321)
+plt.title('Loop Shape Parameter vs. Cycle')
+plt.plot(df['Cycle'],df['Loop Shape Parameter_trapz'],'k-',label='Loop shape parameter')
+plt.ylabel('Loop shape parameter')
+plt.xlabel('Cycle [-]')
+plt.ylim(0,1)
+plt.xscale('log')
+plt.legend(loc='best')
+
+plt.show()
