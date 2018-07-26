e538faab59260e441d8cc717063f55b1e0127206
sision0816 <chenwen816@gmail.com>
2018-07-26 17:53:16 +0200

    Update 20170726strainEnergyDensityCalculation.py

--- 20170726strainEnergyDensityCalculation.py
+++ 20170726strainEnergyDensityCalculation.py
@@ -13,13 +13,13 @@ from pandas import DataFrame, read_csv,read_excel
 # input the read file name and write file name
 #==============================================================================
 #print 'Input the file name you want to analysis:' 
-readFile_timeSequence = 'experimentTimeData.csv' # in with csv format and in list
+readFile_timeSequence = 'Nr37_experiment_timeData.csv' # in with csv format and in list
 #print 'Input the file name you want to read of hysteresisMechanicalAnalysis result:' 
-readFile_hysteresisMechanicalAnalysis = 'experimentHysteresisMechanicalAanalysis_20_0.997.csv' # in with csv format and in list
+readFile_hysteresisMechanicalAnalysis = 'experimentHysteresisMechanicalAanalysis_40_0.99_40_0.99.csv' # in with csv format and in list
 #print 'Input the file name of first cycle:' 
-readFile_firstCycle = 'Nr17_cycle1_1kN.csv' 
+readFile_firstCycle = 'Nr37_cycle1_1kN.csv' 
 #print 'Input the file name you want to write for the strain enenergy calculation result:'
-writeFile_strainEnergyCalculationResult = 'experimentStrainEnergyCalculationResult1.csv'
+writeFile_strainEnergyCalculationResult = 'experimentStrainEnergyCalculationResult.csv'
 
 #==============================================================================
 # read the csv data file into dataframe
@@ -34,7 +34,7 @@ mechanicalAnalysisData = pd.read_csv(readFile_hysteresisMechanicalAnalysis,sep='
 #==============================================================================
 
 firstCycleFile = pd.read_csv(readFile_firstCycle,sep=';')
-extensormeterInitialValue = firstCycleFile['Dehnung_fullrange_SH46 mm'][0]
+extensormeterInitialValue = firstCycleFile['Sandner_1983 mm'][0]
 crossSection = 58.905
 gageLength = 15
 #yieldStrain = 0.0005
@@ -44,7 +44,7 @@ gageLength = 15
 #==============================================================================
 
 df['Stress MPa']=pow(10,3)*df['Kraft kN']/crossSection #Stress unite in MPa
-df['Strain']=(df['Dehnung_fullrange_SH46 mm']-extensormeterInitialValue)/(gageLength-extensormeterInitialValue)
+df['Strain']=(df['Sandner_1983 mm']-extensormeterInitialValue)/(gageLength-extensormeterInitialValue)
 
 #==============================================================================
 # itterate each cycle and calculate the strain energy
@@ -88,19 +88,34 @@ for cycle in range (1, maxCycle-1):#the maxCycle-1 cycle may cause error in the 
 #==============================================================================
 #  intgrate via simps
 #==============================================================================
-   energy_upperRight_simps = scipy.integrate.simps(yyUpperRight_array,xxUpperRight_array,even='avg')
-   energy_upperLeft_simps = scipy.integrate.simps(yyUpperLeft_array,xxUpperLeft_array,even='avg')
-   energy_totalCyclicStrain_simps = energy_upperRight_simps + energy_upperLeft_simps # total cyclic strain energy
-   energy_nonplasticStrain_simps = np.absolute(scipy.integrate.simps(np.absolute(yylower_array),np.absolute(xxlower_array),even='avg'))#the integration would be negative value, should get absolute value for area
-   energy_plasticStrain_simps = energy_totalCyclicStrain_simps - energy_nonplasticStrain_simps
+   try:
+       energy_upperRight_simps = scipy.integrate.simps(yyUpperRight_array,xxUpperRight_array,even='avg')
+       energy_upperLeft_simps = scipy.integrate.simps(yyUpperLeft_array,xxUpperLeft_array,even='avg')
+       energy_totalCyclicStrain_simps = energy_upperRight_simps + energy_upperLeft_simps # total cyclic strain energy
+       energy_nonplasticStrain_simps = np.absolute(scipy.integrate.simps(np.absolute(yylower_array),np.absolute(xxlower_array),even='avg'))#the integration would be negative value, should get absolute value for area
+       energy_plasticStrain_simps = energy_totalCyclicStrain_simps - energy_nonplasticStrain_simps
 #==============================================================================
 #    intgrate via trapz
 #==============================================================================
-   energy_upperRight_trapz = scipy.integrate.trapz(yyUpperRight_array,xxUpperRight_array,axis=-1)
-   energy_upperLeft_trapz = scipy.integrate.trapz(yyUpperLeft_array,xxUpperLeft_array,axis=-1)
-   energy_totalCyclicStrain_trapz = energy_upperRight_trapz + energy_upperLeft_trapz # total cyclic strain energy
-   energy_nonplasticStrain_trapz = np.absolute(scipy.integrate.trapz(np.absolute(yylower_array),np.absolute(xxlower_array),axis=-1))#the integration would be negative value, should get absolute value for area
-   energy_plasticStrain_trapz = energy_totalCyclicStrain_trapz - energy_nonplasticStrain_trapz
+       energy_upperRight_trapz = scipy.integrate.trapz(yyUpperRight_array,xxUpperRight_array,axis=-1)
+       energy_upperLeft_trapz = scipy.integrate.trapz(yyUpperLeft_array,xxUpperLeft_array,axis=-1)
+       energy_totalCyclicStrain_trapz = energy_upperRight_trapz + energy_upperLeft_trapz # total cyclic strain energy
+       energy_nonplasticStrain_trapz = np.absolute(scipy.integrate.trapz(np.absolute(yylower_array),np.absolute(xxlower_array),axis=-1))#the integration would be negative value, should get absolute value for area
+       energy_plasticStrain_trapz = energy_totalCyclicStrain_trapz - energy_nonplasticStrain_trapz
+#==============================================================================
+#    Incase they upper or lower array is empty can lead to IndexError
+#==============================================================================
+   except IndexError:
+       energy_upperRight_simps = None
+       energy_upperLeft_simps = None
+       energy_totalCyclicStrain_simps = None
+       energy_nonplasticStrain_simps = None
+       energy_plasticStrain_simps = None
+       energy_upperRight_trapz = None
+       energy_upperLeft_trapz = None
+       energy_totalCyclicStrain_trapz = None # total cyclic strain energy
+       energy_nonplasticStrain_trapz = None#the integration would be negative value, should get absolute value for area
+       energy_plasticStrain_trapz = None
 #==============================================================================
 #    elastic strain energy
 #==============================================================================
@@ -111,12 +126,18 @@ for cycle in range (1, maxCycle-1):#the maxCycle-1 cycle may cause error in the 
 #==============================================================================
 #    loop shape parameter
 #==============================================================================
-   squareArea = (strainMax-strainMin)*(stressMax-stressMin)
-   shapeParameter_simps = energy_plasticStrain_simps[0]/squareArea
-   shapeParemeter_trapz = energy_plasticStrain_trapz[0]/squareArea
+   try:
+       squareArea = (strainMax-strainMin)*(stressMax-stressMin)
+       shapeParameter_simps = energy_plasticStrain_simps[0]/squareArea
+       shapeParemeter_trapz = energy_plasticStrain_trapz[0]/squareArea
+   except TypeError: 
+       shapeParameter_simps = None
+       shapeParemeter_trapz = None
    #output the analysis result
-   dfOutput.loc[len(dfOutput)] = [cycle,stressMax,stressMin,stressAmp,stressMean,strainMax,strainMin,strainAmp,strainMean,energy_totalCyclicStrain_simps[0],energy_plasticStrain_simps[0],energy_totalCyclicStrain_trapz[0],energy_plasticStrain_trapz[0],energy_elasticStrain,shapeParameter_simps,shapeParemeter_trapz]
-   
+   try:
+       dfOutput.loc[len(dfOutput)] = [cycle,stressMax,stressMin,stressAmp,stressMean,strainMax,strainMin,strainAmp,strainMean,energy_totalCyclicStrain_simps[0],energy_plasticStrain_simps[0],energy_totalCyclicStrain_trapz[0],energy_plasticStrain_trapz[0],energy_elasticStrain,shapeParameter_simps,shapeParemeter_trapz]
+   except TypeError:
+      dfOutput.loc[len(dfOutput)] = None
 #==============================================================================
 #  write dataframe to  csv
 #==============================================================================
