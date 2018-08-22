121ee02cde475638d711049e6eeb9c332eabc759
sision0816 <chenwen816@gmail.com>
2018-08-22 11:17:19 +0200

    Create 20180821timeSequenceDataTrueStressStrainCalcultor.py

---
+++ 20180821timeSequenceDataTrueStressStrainCalcultor.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 17 14:09:39 2017
+
+@author: chen_w1
+"""
+
+import pandas as pd
+import numpy as np
+from pandas import DataFrame, read_csv
+#==============================================================================
+# input the read file name and write file name
+#==============================================================================
+print 'Input the file name you want to analysis:' 
+readFile_original = 'SKA200_08_experiment_timeData.csv' # in with csv format and in list
+readFile_firstCycle = 'SKA200_08_cycle1_1kN.csv'
+print 'Input the file name you want to write the after filtered time sequence data:'
+writeFile_timeData_withTrueStressStrain = 'SKA200_08_experiment_timeData_withTrueStresStrain.csv'
+
+#==============================================================================
+# #input data file
+#==============================================================================
+
+df=pd.read_csv(readFile_original,sep=';')
+df_firstCycle = pd.read_csv(readFile_firstCycle, sep=',')
+
+#==============================================================================
+# calculate the Eng. and True stress strain
+#==============================================================================
+df['Engineering stress MPa'] = df['Load kN']*1000/50.2655
+df['Engineering strain'] = (df['Strain mm']-df_firstCycle['Strain mm'][0])/(15+df_firstCycle['Strain mm'][0])
+df['True stress MPa']= df['Engineering stress MPa']*(1+df['Engineering strain'])
+df['True strain'] = np.log(1+df['Engineering strain'])
+
+print 'finish'
